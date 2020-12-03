92154e376ddb49ab122a0844f8145031059c6d79
Dev Little C <devcarlos.cesar@gmail.com>
2020-12-03 12:49:53 -0300

    Adicionando arquivo

---
+++ aula05.js
@@ -0,0 +1,15 @@
+function dados () {
+    console.log("Impressão de dados com function");
+}
+
+dados()
+
+const dados2 = function (){
+    console.log("Impressão de dados com function depois do sinal de =")
+}
+
+dados2();
+
+let dados3 = () => {
+    console.log("Impressão de dados com arrow function")
+}
